887a35730d603bff43234f851d0ed4cf40fae2c9
sjh910805@gmail.com <sjh910805@gmail.com>
2023-01-27 12:34:29 +0900

    feat: 주문, 주문 상태 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -28,4 +28,27 @@ public class OrderItem {
     private Order order; // 주문
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
+
+    //== 생성 메서드 ==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+//        if(item.getStockQuantity() < count) {
+//            throw new IllegalStateException("재고가 부족합니다용");
+//        }
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+    //== 비즈니스 로직 ==//
+    /** 주문 취소 */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+    // == 조회 로직 == //
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
